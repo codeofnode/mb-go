33040b11d93089852cc43a39bf9dc817238187c7
Ramesh Kumar <codeofnode@gmail.com>
2020-06-18 17:02:45 +0000

    SERVER- todo server star

--- dir/main.go
+++ dir/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"{{DOCKER_APP_PATH}}/pkg/app"
+	"{{DOCKER_APP_PATH}}/server"
 	"os"
 )
 
 func main() {
-	app.Init(os.Args)
+	portStr, exists := os.LookupEnv("PORT")
+	if !exists {
+		portStr = "5823"
+	}
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		panic("Env variable PORT is an invalid number!")
+	}
+	if port < 1 || port > 65535 {
+		panic("Env variable PORT should be between 1 and 65535!")
+	}
+	server.Start(port)
 }
